29831303c8207a2128e0bcdb8f164222bfe5309a
owen <stensonowen@gmail.com>
2016-12-06 02:35:07 -0500

    add some documentation to NonContiguous

--- Project2/NonContiguous.cpp
+++ Project2/NonContiguous.cpp
@@ -1,26 +1,32 @@
+/* Non-contiguous memory management
+ *  Implementation mostly handled via Contiguous namespace
+ *  Memory doesn't have to be contiguous if we can keep track of 
+ *      the mapping between logical and physical addresses, which is
+ *      trivial in a vector (which maps unsigned ints to unsigned ints)
+ *  The lookup table isn't actually necessary for this trivial 
+ *      implementation (it doesn't require the memory to actually do
+ *      anything), but it is shown.
+ */
+
 #include "NonContiguous.hpp"
 
 using namespace Contiguous;
 
 NonContiguous::NonContiguous() : MemoryManager("") {
-	//std::cout << m.size() << "!!!\n";
+    //set up our table for access (we know the exact length)
 	table.resize(m.size());
 	lastUsed = 0;
 }
 NonContiguous::~NonContiguous() {}
 
 void NonContiguous::addProcP(char p, int n) {
-	//insert wherever possible
-	//std::cout << t << "Process " << p << " arrived (requires " 
-	//        << n << " frames)\n";
-
-	//int last_used = 0;
 	lastUsed = 0;
-	for (unsigned int i=0; i<n; ++i) {
-		//lastUsed = m.findNextFreeChunk(lastUsed);
+    //request MemoryManager for one frame at a time
+    //keep track of the proper mapping in self.table[i]
+	for (/*unsigned*/ int i=0; i<n; ++i) {
 		lastUsed = m.findNextFit(lastUsed, 1).first;
 		if (lastUsed < 0) {
-			std::cout << "No more memory! Mama mia!\n";
+            perror("Not enough memory; only call NonContiguous::addProcP if we have the memory");
 		}
 		m.allocateMemory(lastUsed, 1, p);
 		table[i] = lastUsed;
@@ -28,9 +34,11 @@ void NonContiguous::addProcP(char p, int n) {
 }
 
 void NonContiguous::removeProcP(char p) {
-	//removeProcP(p);
-	//std::cout << t << "Process " << p << " removed:\n";
+    //we don't even need our table of data because MemoryManager removes 
+    // processes by checking every frame
 	m.freeMem(p);
-	//m.printMemory();
-
+    //zero out our table (just good practice)
+    for (unsigned int i=0; i<table.size(); ++i) {
+        table[i] = 0;
+    }
 }
